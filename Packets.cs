cbddd23961e42587f1f6f3962d20cff35309d0ea
Gurunath Sankar <guru.csest@gmail.com>
2020-04-21 17:23:48 +0530

    Create Packets.cs

---
+++ Packets.cs
@@ -0,0 +1,9 @@
+namespace NMFConvertionBasedOnPython
+{
+    internal class Packets
+    {
+        public int Compression { get; set; }
+        public int StreamId { get; set; }
+        public string RawChunkData { get; set; }
+    }
+}
