b94fc884bb3762f2e5d8f4cd913c72d1d56b1ff7
gurunath17 <gurunath.sankar@genesys.com>
2020-04-22 15:00:56 +0530

    removed old code

--- Packets.cs
+++
@@ -1,9 +0,0 @@
-namespace NMFConvertionBasedOnPython
-{
-    internal class Packets
-    {
-        public int Compression { get; set; }
-        public int StreamId { get; set; }
-        public string RawChunkData { get; set; }
-    }
-}
